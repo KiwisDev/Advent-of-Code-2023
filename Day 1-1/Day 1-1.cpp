c03032c9980921d0d3f2ba5ad97cb639fb5a7608
KiwisDev <gaelvolatier@gmail.com>
2023-12-01 09:34:27 +0100

    Add project files.

---
+++ Day 1-1/Day 1-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream fs("inputs/puzzle.txt");
+
+    string line;
+	int total = 0;
+	while (getline(fs, line)) {
+		string num = "";
+		
+		int i = 0;
+		while (num.length() < 1 && i < line.length()) {
+			if (isdigit(line[i]))
+				num.push_back(line[i]);
+			i++;
+		}
+
+		i = line.length()-1;
+		while (num.length() < 2 && i >= 0) {
+			if (isdigit(line[i]))
+				num.push_back(line[i]);
+			i--;
+		}
+
+		cout << line << " " << stoi(num) << endl;
+		total += stoi(num);
+	}
+
+	cout << total;
+
+	return 0;
+}
